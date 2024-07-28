aabefe7193e33c97aea8e9df9008d4aaf681e6a1
Kaya-Sem <kayasemvc@gmail.com>
2024-07-28 09:15:42 +0200

    update

---
+++ InfixToRPN.java
@@ -0,0 +1,27 @@
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Convert tokens in infix notation to prefix notation.
+ * @author Kayasem
+ */
+public class InfixToRPN {
+    private static final Map<String, Integer> precedenceMap = Map.of(
+            "!", 3,
+            "&&", 2,
+            "||", 1,
+            "->", 0
+    );
+
+    /**
+     * Converts an array of infix tokens and returns a list of tokens in RPN.
+     *
+     * @param infixTokens the array of tokens in infix notation
+     * @return the list of tokens in RPN
+     * @see <a href="https://en.wikipedia.org/wiki/Reverse_Polish_notation">Reverse Polish notation</a>
+     */
+    public static List<String> convertToRPN(String[] infixTokens) {
+        return null;
+
+    }
+}
--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        String expression = "1 -> 0";
+        String expression = "1 0 ->";
         Expression parsedExpression = PropositionParser.parse(expression);
         boolean result = parsedExpression.evaluate();
         System.out.println("Result: " + result); // Should print: Result: false
+        System.out.println("(¬A ∧ B) ∨ B → C");
     }
 }
